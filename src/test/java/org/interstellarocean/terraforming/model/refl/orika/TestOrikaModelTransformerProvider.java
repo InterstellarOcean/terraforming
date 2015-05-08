9e356469f22c3fa91d136972228618b35095492a
Dariusz Wakulinski <dariuswak@gmail.com>
2015-05-08 23:28:57 +0200

    Relates #1, Added orika reflection model transformer

---
+++ src/test/java/org/interstellarocean/terraforming/model/refl/orika/TestOrikaModelTransformerProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2015 The Authors
+ *
+ * https://www.gnu.org/licenses/lgpl-3.0-standalone.html
+ */
+package org.interstellarocean.terraforming.model.refl.orika;
+
+import ma.glasnost.orika.BoundMapperFacade;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.metadata.ClassMapBuilder;
+import org.interstellarocean.terraforming.reflection.ReflectionModelTransformerProvider;
+
+/**
+ * @author Dariusz Wakuliński
+ */
+public class TestOrikaModelTransformerProvider implements ReflectionModelTransformerProvider {
+
+	private final MapperFactory mapperFactory;
+
+	// CDI: @Autowired, @Inject, etc.
+	public TestOrikaModelTransformerProvider(MapperFactory mapperFactory) {
+		this.mapperFactory = mapperFactory;
+	}
+
+	@Override
+	public boolean provides(Class<?> library) {
+		return ma.glasnost.orika.MapperFacade.class.equals(library);
+	}
+
+	@Override
+	public <SOURCE, DESTINATION> Builder<SOURCE, DESTINATION> builder() {
+		return sourceType -> destinationType -> excludeFieldNames -> () -> {
+
+			ClassMapBuilder<SOURCE, DESTINATION> builder = mapperFactory.classMap(sourceType, destinationType);
+			excludeFieldNames.forEach(fieldName -> builder.exclude(fieldName));
+			builder.byDefault().register();
+
+			BoundMapperFacade<SOURCE, DESTINATION> reflectionModelTransformer = mapperFactory.getMapperFacade(sourceType, destinationType);
+			return source -> destination -> {
+				reflectionModelTransformer.map(source, destination);
+				return destination;
+			};
+
+		};
+	}
+
+}
